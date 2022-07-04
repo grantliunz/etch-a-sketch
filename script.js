1671282e5eee90a3cae7189c484f7f5112757357
Grant <gliu871@aucklanduni.ac.nz>
2022-07-04 18:44:40 +1200

    updatd grid sizing and draggable bug

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.body.ondragstart = () => false;
+
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
@@ -8,7 +10,7 @@ let gridSize = 32;
 
 let gridContainer = document.createElement('div');
 gridContainer.classList.add('gridContainer');
-gridContainer.style.cssText = 'background: white; margin:auto; margin-top:20px; margin-bottom:20px; width:960px; height:960px; display: grid; ';
+gridContainer.style.cssText = 'background-color: white; margin:auto; margin-top:20px; margin-bottom:20px; width:960px; height:960px; display: grid; ';
 container.appendChild(gridContainer);
 
 
@@ -24,10 +26,10 @@ function updateGrid(gridSize) {
     for (let i = 0; i < gridSize * gridSize; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
-        gridItem.style.cssText = 'border: 1px solid rgba(0, 0, 0, 0.1); border-style: solid solid none none; background:white;'
+        gridItem.style.cssText = 'border: 1px solid rgba(0, 0, 0, 0.1); background-color:white;'
+        gridItem.setAttribute('draggable', 'false');
         gridItem.addEventListener('mouseover', clickGrid);
         gridItem.addEventListener('mousedown', clickGrid);
-
         gridContainer.appendChild(gridItem);
     }
     container.appendChild(gridContainer);
@@ -40,7 +42,9 @@ updateGrid(16);
 
 function clickGrid(e) {
     if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.background = 'black';
+    if (e.target.style.backgroundColor == '#777') return
+    e.target.style.backgroundColor = '#777';
+    console.log('tset1');
 }
 
 const optionsContainer = document.querySelector("#optionsContainer");
@@ -61,6 +65,14 @@ sizeInput.style.cssText = "width:150px; height:20px; margin:10px 0 2px 10px;"
 sizeInput.setAttribute('placeholder', 'Enter grid size (1-100)');
 sizeInput.setAttribute('type', 'number');
 sizeInput.setAttribute('id', 'sizeInput');
+sizeInput.setAttribute('onkeypress', 'enterSizeInput(event)');
+function enterSizeInput(ele) {
+    if (ele.keyCode === 13) {
+        let size = document.getElementById('sizeInput').value;
+        document.getElementById('sizeInput').value = null;
+        if (size > 0 & size < 101) updateGrid(size);
+    }
+}
 optionsContainer.appendChild(sizeInput);
 
 const sizeButton = document.createElement('button');
