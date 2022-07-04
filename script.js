8040ce3cd9d53b3c1f35350c62530db51de4acb6
Grant <gliu871@aucklanduni.ac.nz>
2022-07-04 16:58:49 +1200

    added size update based on user input

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
-let mouseDown = 0;
-document.body.onmousedown = function () {
-    mouseDown = 1;
-}
-document.body.onmouseup = function () {
-    mouseDown = 0;
-}
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 const container = document.querySelector('#container');
 
-let gridSize = 100;
-// let mouseDown = false;
+let gridSize = 32;
 
-const gridContainer = document.createElement('div');
+let gridContainer = document.createElement('div');
 gridContainer.classList.add('gridContainer');
 gridContainer.style.cssText = 'background: white; margin:auto; margin-top:20px; margin-bottom:20px; width:960px; height:960px; display: grid; ';
-gridContainer.style.gridTemplateColumns = 'auto '.repeat(gridSize);
-// gridContainer.addEventListener('mousedown', () => { mouseDown = true });
-// gridContainer.addEventListener('mouseup', () => { mouseDown = false });
-
 container.appendChild(gridContainer);
 
 
+function updateGrid(gridSize) {
+    gridContainer = container.removeChild(gridContainer);
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('gridItem');
-    gridItem.style.cssText = 'border: 1px solid rgba(0, 0, 0, 0.5); border-style: solid solid none none; background:white;'
-    gridItem.addEventListener('mouseenter', () => {
-        if (mouseDown) { gridItem.style.background = 'black'; }
+    gridContainer.style.gridTemplateColumns = 'auto '.repeat(gridSize);
 
-    });
-    gridItem.addEventListener('mousedown', () => {
-        gridItem.style.background = 'black';
 
-    });
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        gridItem.style.cssText = 'border: 1px solid rgba(0, 0, 0, 0.1); border-style: solid solid none none; background:white;'
+        gridItem.addEventListener('mouseover', clickGrid);
+        gridItem.addEventListener('mousedown', clickGrid);
 
-    gridContainer.appendChild(gridItem);
+        gridContainer.appendChild(gridItem);
+    }
+    container.appendChild(gridContainer);
+
+}
+
+updateGrid(16);
+
+
+
+function clickGrid(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.background = 'black';
 }
 
 const optionsContainer = document.querySelector("#optionsContainer");
 
 const reset = document.createElement('button');
-reset.style.cssText = "width:100px; height:50px"
+reset.style.cssText = "width:100px; height:50px; margin:10px"
 reset.textContent = "Reset";
 reset.addEventListener('click', () => {
     const gridItems = document.querySelectorAll('.gridItem');
-    console.log('test1');
     gridItems.forEach((item) => {
         item.style.background = 'white';
-        console.log('test2');
     });
 });
 optionsContainer.appendChild(reset);
+
+let sizeInput = document.createElement('input');
+sizeInput.style.cssText = "width:150px; height:20px; margin:10px 0 2px 10px;"
+sizeInput.setAttribute('placeholder', 'Enter grid size (1-100)');
+sizeInput.setAttribute('type', 'number');
+sizeInput.setAttribute('id', 'sizeInput');
+optionsContainer.appendChild(sizeInput);
+
+const sizeButton = document.createElement('button');
+sizeButton.style.cssText = "width:100px; height:20px; margin-left:10px;";
+sizeButton.textContent = "Update";
+sizeButton.addEventListener('click', () => {
+    let size = document.getElementById('sizeInput').value;
+    document.getElementById('sizeInput').value = null;
+    if (size > 0 & size < 101) updateGrid(size)
+});
+
+optionsContainer.appendChild(sizeButton);
+
